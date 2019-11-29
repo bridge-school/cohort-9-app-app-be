a4d2c25c88c4751ce3ff0371590c3c386363c688
sogol <sogol.naseri@gmail.com>
2019-11-28 19:16:47 -0500

    addedcors

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,41 @@
 const express = require("express");
 
 const { healthRouter } = require("../routes/health/health.router");
-const { applicationsRouter } = require("../routes/applications/applications.router");
-const { studentApplicationsRouter } = require("../routes/studentApplications/studentApplications.router");
+const {
+  applicationsRouter
+} = require("../routes/applications/applications.router");
+const {
+  studentApplicationsRouter
+} = require("../routes/studentApplications/studentApplications.router");
 // const { <newRoute>Router } = require("../routes/<newRoute>/<newRoute>.router");
 
-const router = express.Router();
-router.use("/health", healthRouter);
-router.use("/applications", applicationsRouter);
-router.use("/applications/students", studentApplicationsRouter);
+const app = express();
 // router.use("/<newRoute>", <newRoute>Router);
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-module.exports = router;
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.route("/health", healthRouter);
+app.route("/applications", applicationsRouter);
+app.route("/applications/students", studentApplicationsRouter);
+module.exports = app;
