676cff4be127504eec381222e88b1feb2b9720c6
sogol <sogol.naseri@gmail.com>
2019-12-08 14:43:22 -0500

    added filter to get application

--- src/routes/applications/applications.controller.js
+++ src/routes/applications/applications.controller.js
@@ -1,28 +1,62 @@
 const { logger } = require("../../utils/logger");
 const db = require("../../db/index.js");
+const moment = require("moment");
 
 const getApplicationsController = (req, res) => {
   // Applications is the name of the collection I created in firebase
-  db.collection("Applications")
-    .get()
-    .then(snapshot => {
-      console.log("******************");
-      res.json({
-        cohort_apps: snapshot.docs.map(doc => {
-          return {
-            id: doc.id,
-            ...doc.data()
-          };
-        })
+
+  console.log(req.query);
+  //If the query has parameter with open =1 then filter the data to user
+  if (req.query && req.query.open && req.query.open === "1") {
+    const today = moment().format();
+
+    db.collection("Applications")
+      //First we filter open apps
+      .where("dateOpen", "<=", today)
+      .get()
+      //Then check if application is not closed
+      .then(snapshot => {
+        console.log("******************");
+        const result = snapshot.docs.filter(doc => {
+          return doc.data().dateClose >= today;
+        });
+
+        res.json({
+          cohort_apps: result.map(doc => {
+            return {
+              id: doc.id,
+              ...doc.data()
+            };
+          })
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.json(err);
       });
-    })
-    .catch(err => {
-      console.log(err);
-      res.json(err);
-    });
+  } else {
+    //if query is without parameter then show all cohort to user
+    db.collection("Applications")
+      .get()
+      .then(snapshot => {
+        console.log("******************");
+        res.json({
+          cohort_apps: snapshot.docs.map(doc => {
+            return {
+              id: doc.id,
+              ...doc.data()
+            };
+          })
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.json(err);
+      });
+  }
 };
 
-const postApplicationController = (req, res) => {  
+const postApplicationController = (req, res) => {
   db.collection("Applications")
     .add({
       ...req.body
@@ -43,7 +77,3 @@ module.exports = {
   getApplicationsController,
   postApplicationController
 };
-
-// return res.json({
-//     data: snapshot.docs
-//   });
